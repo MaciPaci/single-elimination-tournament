b4c4eea3da792f8a8d379c8a46311a54beb3e4ee
maciej.stachowiak <maciej.stachowiak@olx.pl>
2022-06-12 19:30:04 +0200

    Added tournament list table with redirect buttons

--- project/tournaments.py
+++ project/tournaments.py
@@ -9,13 +9,13 @@ tournament = Blueprint('tournament', __name__)
 
 @tournament.route('/tournament/create')
 @login_required
-def create_tournament():
+def create():
     return render_template('tournament_create.html')
 
 
 @tournament.route('/tournament/create', methods=['POST'])
 @login_required
-def create_tournament_post():
+def create_post():
     name = request.form.get('name')
     start_date = request.form.get('start_date')
     player_count = request.form.get('player_count')
@@ -26,16 +26,19 @@ def create_tournament_post():
     db.session.add(new_tournament)
     db.session.commit()
 
-    return redirect(url_for('tournament.manage_tournament', tournament_id=id))
+    return redirect(url_for('tournament.manage', tournament_id=id))
 
 
 @tournament.route('/tournament/<string:tournament_id>')
 @login_required
-def manage_tournament(tournament_id):
+def manage(tournament_id):
     return render_template('tournament_manage.html', tournament_id=tournament_id)
 
 
 @tournament.route('/tournament/list')
-def list_tournament():
+def list():
+    if request.form.get("manage_button"):
+        # return redirect(url_for('tournament.manage_tournament', tournament_id=
+        print(request.form.get("manage_button"))
     tournaments_list = Tournament.query.all()
     return render_template('tournament_list.html', list_of_tournaments=tournaments_list)
