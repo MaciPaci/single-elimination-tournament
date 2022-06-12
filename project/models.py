3c0b81c10b1cde6d36acedec827cea612d93f084
maciej.stachowiak <maciej.stachowiak@olx.pl>
2022-06-12 21:51:19 +0200

    Torunament management layout

--- project/models.py
+++ project/models.py
@@ -15,3 +15,9 @@ class Tournament(db.Model):
     name = db.Column(db.String(1000))
     start_date = db.Column(db.String(100))
     player_count = db.Column(db.String(10))
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tournament_id = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
